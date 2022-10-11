01bb893e8b9e17c4ae0127c067d1c27e9f7a17de
unknown <adam2510025@gmail.com>
2022-10-11 11:26:27 +0700

    commsult day 2

--- atma-jaya/src/App.js
+++ atma-jaya/src/App.js
@@ -9,29 +9,81 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      name: "Adam Christoper",
+      names: ["Adam Christoper"],
+      name: "",
       age: 20,
       hobby: "Playing Games",
     };
   }
 
   componentDidUpdate() {
-    console.log("App is updated!")
+    console.log("App is updated!");
   }
 
-  incrementAge = ()=> {
-    this.setState({age: this.state.age + 1})
-  }
-  decrementAge= ()=> {
-    this.setState({age: this.state.age - 1})
-  }
+  // ada 2 cara untuk jalanin function add dan substract
+  // incrementAge() {
+  //   this.setState({age: this.state.age + 1})
+  // }
+  // decrementAge() {
+  //   this.setState({age: this.state.age - 1})
+  // }
+
+  incrementAge = () => {
+    this.setState({ age: this.state.age + 1 });
+  };
+  decrementAge = () => {
+    this.setState({ age: this.state.age - 1 });
+  };
+
+  addName = () => {
+    this.setState({
+      names: [...this.state.names, this.state.name],
+    });
+  };
+
+  deleteName = (names) => () => {
+    const result = this.state.names.filter(name => {
+      return name !== names
+    })
+    this.setState({names: result})
+  };
 
   render() {
     return (
       <div className="App">
+        {this.state.names.map((names) => {
+          return (
+            <div>
+              <ul className="testing">
+                <li>
+                  <span>
+                    <label className="label-nama">{names}</label>
+                    <button onClick={this.deleteName(names)}>Delete</button>
+                  </span>
+                </li>
+              </ul>
+            </div>
+          );
+        })}
         <h1>Hello my Name is {this.state.name}</h1>
         <h3>My hobby is {this.state.hobby}</h3>
-        <AgeComponent age={this.state.age} incrementAge={this.incrementAge} decrementAge={this.decrementAge}/>
+        {/* untuk metode function biasa */}
+        {/* <AgeComponent age={this.state.age} incrementAge={this.incrementAge.bind(this)} decrementAge={this.decrementAge.bind(this)}/> */}
+        {/* jika meggunakan metode arrow function */}
+        <AgeComponent
+          age={this.state.age}
+          incrementAge={this.incrementAge}
+          decrementAge={this.decrementAge}
+        />
+
+        {/* mengubah data dalam state dari input */}
+        <input
+          value={this.state.name}
+          onChange={(event) => this.setState({ name: event.target.value })}
+        />
+        <br />
+        <button onClick={this.addName}>Add Name</button>
+        <button onClick={this.deleteName}>Delete</button>
       </div>
     );
   }
